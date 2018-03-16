4accff7bcd544c4f2318e63b4cf8c079c5b2cb3a
yejiawei1234 <yejiawei123@gmail.com>
2018-03-16 10:20:23 +0800

    add base.html

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,15 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'yejiawei'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'jiajia'},
+            'body': 'what a lovely day'
+        },
+        {
+            'author': {'username': 'weiwei'},
+            'body': 'the day we love'
+        }
+
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
